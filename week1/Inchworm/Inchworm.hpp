76df13d0a01f7ac3619f1be392d381393aac4498
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-08 11:54:13 +0930

    1

--- week1/Inchworm/Inchworm.hpp
+++ week1/Inchworm/Inchworm.hpp
@@ -5,17 +5,7 @@ using namespace std;
 class Inchworm
 {
   public:
-  int lunchtime(int branch, int rest, int leaf)
-  {
-    int res = 0;
-    for (int i = 0; i <= branch; i += rest)
-    {
-        if(i%leaf == 0)
-        {
-            res++;
-        }
-    }
-    
-    return res;  // return your result
+  int lunchtime(int branch, int rest, int leaf) {
+    return (int) (branch / gcl(rest, leaf)) + 1;
   }
 };
