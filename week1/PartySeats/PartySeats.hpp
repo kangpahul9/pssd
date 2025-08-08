3faccd41b6df449324015c073c357c1a6d67bdee
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-08 14:54:33 +0930

    l

--- week1/PartySeats/PartySeats.hpp
+++ week1/PartySeats/PartySeats.hpp
@@ -1,44 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
- 
+
 class PartySeats
 {
-  public:
-  	String[] seating(String[] attendees)
- {
-    vector<string> boys;
-    vector<string> girls;
-    vector <string> result;
-    for (auto &i : attendees) {
-        string name = i.substr(i.find(" "));
-        string gender = i.substr(i.find(" ")+1);
-        if(gender=="boy"){
-        boys.push_back(name);}
-        else{
-            girls.push_back(name);
-        }
-    }
-    sort(boys.begin(),boys.end());
-    sort(girls.begin(),girls.end());
-    if(boys.size() != girls.size()){ return result;}
-    if(girls.size() < 2){ return result;}
-    result.push_back("HOST"); int cb = 0; int cg = 0;
-    for(int i = 1; i < attendees.size();i++){
-        if(i == girls.size()+1){
-            result.push_back("HOSTESS");
-            continue;
-        }
-        if (i%2 == 0)
+public:
+    String[] seating(String[] attendees)
+    {
+        vector<string> boys;
+        vector<string> girls;
+        string[] result[attendees.size() + 2];
+        for (auto &i : attendees)
         {
-            result.push_back(boys[cb]);
-            cb++;
+            string name = i.substr(i.find(" "));
+            string gender = i.substr(i.find(" ") + 1);
+            if (gender == "boy")
+            {
+                boys.push_back(name);
+            }
+            else
+            {
+                girls.push_back(name);
+            }
         }
-        else
+        sort(boys.begin(), boys.end());
+        sort(girls.begin(), girls.end());
+        if (boys.size() != girls.size())
         {
-            result.push_back(girls[cb]);
+            return result;
+        }
+        if (girls.size() < 2)
+        {
+            return result;
+        }
+        result.push_back("HOST");
+        int cb = 0;
+        int cg = 0;
+        for (int i = 1; i < attendees.size(); i++)
+        {
+            if (i == girls.size() + 1)
+            {
+                result[i] = "HOSTESS";
+                continue;
+            }
+            if (i % 2 == 0)
+            {
+                result[i] = boys[cb];
+                cb++;
+            }
+            else
+            {
+                result[i] = girls[cg];
+                cg++;
+            }
+        }
+        return result;
     }
-}
-    return result;
- }
 };
