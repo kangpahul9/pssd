7fed34f0685f66848e31e5ed242668a6387fb212
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-11 22:22:15 +0930

    l

--- week1/CircularLine/CircularLine.hpp
+++ week1/CircularLine/CircularLine.hpp
@@ -8,19 +8,17 @@ public:
         vector<int> prefixSum(n + 1, 0);
         for (int i = 0; i < n; ++i) {
             prefixSum[i + 1] = prefixSum[i] + t[i];
-    }
+        }
         int totalTime = prefixSum[n];
         int ans = 0;
 
         for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
-                int travelTime = prefixSum[j] - prefixSum[i];
-                if (travelTime > totalTime / 2) {
-                    ans = max(ans, travelTime);
-                }
+            for (int j = i + 1; j < n; ++j) {  
+                int travelTime = prefixSum[j] - prefixSum[i];   
+                int fastest = min(travelTime, totalTime - travelTime); 
+                ans = max(ans, fastest);
             }
         }
-
         return ans;
     }
 };
