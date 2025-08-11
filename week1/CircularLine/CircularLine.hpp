683e99af9fdd71ff47811680c8762a02286f8180
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-11 22:05:42 +0930

    l

---
+++ week1/CircularLine/CircularLine.hpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class CircularLine {
+public:
+    int longestTravel(vector<int> t) {
+        int n = t.size();
+        vector<int> prefixSum(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefixSum[i + 1] = prefixSum[i] + t[i];
+    }
+        int totalTime = prefixSum[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j <= n; ++j) {
+                int travelTime = prefixSum[j] - prefixSum[i];
+                if (travelTime > totalTime / 2) {
+                    ans = max(ans, travelTime);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
