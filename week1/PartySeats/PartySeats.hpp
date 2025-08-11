f48614a686493e39ad7be52c1f09cb3d8cf1ba54
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-11 20:28:20 +0930

    l

--- week1/PartySeats/PartySeats.hpp
+++ week1/PartySeats/PartySeats.hpp
@@ -1,59 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class PartySeats
-{
+class PartySeats {
 public:
-    vector<string> seating(vector<string> attendees)
-    {
-        vector<string> boys;
-        vector<string> girls;
-        vector<string> result; 
-        for (const string& s : attendees)
-        {
+    vector<string> seating(vector<string> attendees) {
+        vector<string> boys, girls, result;
+
+        for (const string& s : attendees) {
             size_t pos = s.rfind(' ');
+
             string name = s.substr(0, pos);
             string gender = s.substr(pos + 1);
-            if (gender == "boy")
-            {
-                boys.push_back(name);
-            }
-            else
-            {
-                girls.push_back(name);
-            }
+
+            if (gender == "boy") boys.push_back(name);
+            else girls.push_back(name);
         }
         sort(boys.begin(), boys.end());
         sort(girls.begin(), girls.end());
-        if (boys.size() != girls.size())
-        {
-            return result;
-        }
-        if (girls.size() < 2)
-        {
-            return result;
-        }
+        if (boys.size() != girls.size()) return {};
+        if (girls.size() < 2) return {};
+
         result.push_back("HOST");
-        int cb = 0;
-        int cg = 0;
+
+        int cb = 0, cg = 0;
         int total = attendees.size() + 2; 
-        for (int i = 1; i < total; i++)
-        {
-            if (i == total/ 2)
-            {
+
+        for (int i = 1; i < total; ++i) {
+            if (i == total / 2) {              
                 result.push_back("HOSTESS");
                 continue;
             }
-            if (i % 2 == 0)
-            {
-                result.push_back(boys[cb]);
-                cb++;
-            }
-            else
-            {
-                result.push_back(girls[cg]);
-                cg++;
-            }
+            if (i % 2 == 1) result.push_back(girls[cg++]); 
+            else result.push_back(boys[cb++]);  
         }
         return result;
     }
