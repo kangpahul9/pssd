9ceff35a6e5ce4cc57e1e66675f08c1da9f6c579
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-18 19:35:34 +0930

    l

--- week2/ChangingString/ChangingString.hpp
+++ week2/ChangingString/ChangingString.hpp
@@ -10,14 +10,14 @@ class ChangingString
     int nonZero,sum=0;
     for (int i = 0; i < A.size(); i++)
     {
-        di[i]= abs((int)A[i].size() - (int)B[i].size());
+        di[i]= abs((int)A[i] - (int)B[i]);
         sum += di[i];
         if (di[i] > 0) nonZero++;
     }
     
     if(K <=nonZero) {
          for (int i = A.size() - 1; i >= A.size() - K; --i)
-         {sum -= d[i];}
+         {sum -= di[i];}
             return sum;
     }
     else return K-nonZero;
