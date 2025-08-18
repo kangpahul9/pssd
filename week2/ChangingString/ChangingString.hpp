d45533473fd14689c09496facf2dc6aa80543d23
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-18 19:46:33 +0930

    l

--- week2/ChangingString/ChangingString.hpp
+++ week2/ChangingString/ChangingString.hpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
- 
-class ChangingString
-{
-  public:
-  int distance(string A, string B, int K) {
-    vector<int> di(A.size());
-    int nonZero=0;int sum=0;
-    for (int i = 0; i < A.size(); i++)
-    {
-        di[i]= abs((int)A[i] - (int)B[i]);
-        sum += di[i];
-        if (di[i] > 0) nonZero++;
-    }
-    sort(di.begin(), di.end());
-    
-    if(K <=nonZero) {
-         for (int i = A.size() - 1; i >= A.size() - K; --i)
-         {sum -= di[i];}
+
+class ChangingString {
+public:
+    int distance(string A, string B, int K) {
+        int n = (int)A.size();
+        vector<int> d(n);
+        int sum = 0, nonZero = 0;
+
+        for (int i = 0; i < n; ++i) {
+            d[i] = std::abs((int)A[i] - (int)B[i]);
+            sum += d[i];
+            if (d[i] > 0) ++nonZero;
+        }
+
+        sort(d.begin(), d.end()); 
+
+        if (K <= nonZero) {
+            for (int i = n - 1; i >= n - K; --i) sum -= d[i];
             return sum;
+        } else {
+            return K - nonZero;
+        }
     }
-    else return K-nonZero;
-  }
 };
