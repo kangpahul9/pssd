c6694bcde412335f7bcdb37ed1f65df33842e617
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-18 19:39:20 +0930

    l

--- week2/ChangingString/ChangingString.hpp
+++ week2/ChangingString/ChangingString.hpp
@@ -6,14 +6,15 @@ class ChangingString
 {
   public:
   int distance(string A, string B, int K) {
-    vector<int> di;
-    int nonZero,sum=0;
+    vector<int> di(A.size());
+    int nonZero=0;int sum=0;
     for (int i = 0; i < A.size(); i++)
     {
         di[i]= abs((int)A[i] - (int)B[i]);
         sum += di[i];
         if (di[i] > 0) nonZero++;
     }
+    sort(di.begin(), di.end());
     
     if(K <=nonZero) {
          for (int i = A.size() - 1; i >= A.size() - K; --i)
