b18ea6ca1a0483c3beffff362a928d8a482e6b99
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-29 11:28:26 +0930

    l

--- week4/MagicSpell/MagicSpell.hpp
+++ week4/MagicSpell/MagicSpell.hpp
@@ -6,29 +6,21 @@ using namespace std;
 class MagicSpell {
 public:
     string fixTheSpell(string spell){
-        int n = spell.size();int j=0;
+        int n = spell.size();
         string result(n,'0'); string non; 
         for(int i=0; i<n; i++){
-            
-            
             if(spell[i] == 'A' || spell[i] =='Z' ){
-                result[i]=spell[i];
-            }
-            else {
-                result[i]='0';
-                non.push_back(spell[i]);
+                result.push_back(spell[i]);
             }
         }
-        std::reverse(result.begin(), result.end());j=0;
+        std::reverse(result.begin(), result.end());int j=0;
         for (int i = 0; i < n; i++)
         {
-            if (result[i] == '0')
-            {
-                result[i] = non[j];
-                j++;
+            if(spell[i] == 'A' || spell[i] =='Z' ){
+                spell[i] = result[j++];
             }
         }
-        return result;
+        return spell;
     }
 };
 
