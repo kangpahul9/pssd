9172ab70f22f921ea139838c801118f0ddcf05b3
kangpahul9 <pahulpreet2959@gmail.com>
2025-08-29 12:01:10 +0930

    l

--- week4/RugSizes/RugSizes.hpp
+++ week4/RugSizes/RugSizes.hpp
@@ -9,19 +9,16 @@ public:
     int rugCount(int area)
     {
         int count = 0;int j=0;
-        for (int i = 1; i <= area/2; i++)
+        for (int i = 1; i*i <= area; i++)
         {
             if (area%i == 0)
             {
                 j=area/i;
-                if (i==j)
+                if (i!=j && i%2 == 0 && j%2 ==0 )
                 {
-                    count++;
+                    continue;
                 }
-                if (i%2 != 0 && j%2 !=0 )
-                {
-                    count++;
-                }
+                count++;
             }
         }
         return count;
