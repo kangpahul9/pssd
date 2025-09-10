81cecb1f4dad2f2771a2a2c079fe515bdcc9f681
kangpahul9 <pahulpreet2959@gmail.com>
2025-09-10 15:57:18 +0930

    l

--- DownloadingFiles.hpp
+++ DownloadingFiles.hpp
@@ -1,26 +1,24 @@
+//Gemini's code
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
-using namespace std;
 class DownloadingFiles {
 public:
-    double actualTime(vector<string> tasks){
-        double totalTime =0.0; 
-        vector<double> downloadSpeed(tasks.size(), 0.0); 
-        vector<double> downloadTimes(tasks.size(), 0.0);
-        double bandwidth = 0.0;
-        double totalSize = 0.0;
-        for (int i=0; i < tasks.size(); ++i) {
-            size_t pos = tasks[i].find(' ');
-            downloadSpeed[i] = stod(tasks[i].substr(0, pos));
-            downloadTimes[i] = stod(tasks[i].substr(pos+1));
-            bandwidth += downloadSpeed[i];
-            int size = downloadSpeed[i] * downloadTimes[i];
-            totalSize += size;
+    double actualTime(std::vector<std::string> tasks) {
+        double totalData = 0.0;
+        double totalSpeed = 0.0;
+
+        for (const auto& task : tasks) {
+            std::stringstream ss(task);
+            int speed, time;
+            ss >> speed >> time;
+
+            totalData += static_cast<double>(speed) * time;
+            totalSpeed += static_cast<double>(speed);
         }
-        return totalSize/bandwidth;
+
+        return totalData / totalSpeed;
     }
 };
-
-            
