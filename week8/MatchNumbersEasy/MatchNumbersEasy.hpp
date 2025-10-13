f0a068ff137a589b69a6c221254f01dbc9060857
kangpahul9 <pahulpreet2959@gmail.com>
2025-10-13 16:07:26 +1030

    l

--- week8/MatchNumbersEasy/MatchNumbersEasy.hpp
+++ week8/MatchNumbersEasy/MatchNumbersEasy.hpp
@@ -7,28 +7,29 @@ class MatchNumbersEasy {
 public:
     string maxNumber(vector <int> matches, int n)
  {
-        long long maxNumber=0;
-        long long currMax=0;
-        long long left=n;
+                string maxNum = "0";
+
         for (int i = matches.size()-1; i >= 0; i--)
         {
             if (matches[i] > n) continue;
-            left=n-matches[i];
-            currMax=i;
+            int left = n - matches[i];
+            string curr = to_string(i); 
             for (int j = matches.size()-1; j >= 0; j--)
             {
-                int tempLeft = left;
-                long long tempNum = currMax;
-                while (tempLeft >= matches[j])
-                {
-                    tempNum=tempNum*10+j;
-                maxNumber=max(tempNum,maxNumber);
-                tempLeft-=matches[j];
+                 int tempLeft = left;
+                string tempNum = curr;
+                while (tempLeft >= matches[j]) {
+                    tempNum += to_string(j);  
+                    tempLeft -= matches[j];
+                    if (isBigger(tempNum, maxNum))
+                        maxNum = tempNum;
                 }
             }
         }
-        string result = "";
-        result=std::to_string(maxNumber);
-        return result;
+        return maxNum;
+    }
+bool isBigger(const string &a, const string &b) {
+        if (a.size() != b.size()) return a.size() > b.size();
+        return a > b;
     }
 };
